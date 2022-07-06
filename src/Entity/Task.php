8d5e996ac9f493d469275b31eae08e2f446b56d3
JCMonnet <jcmonnet0408@gmail.com>
2022-07-06 11:49:16 +0200

    afficher que les task du user conecté

--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -3,11 +3,12 @@
 namespace App\Entity;
 
 use App\Repository\TaskRepository;
+use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass:TaskRepository::class)]
 class Task
 {
-    #[ORM\Id]
+#[ORM\Id]
 #[ORM\GeneratedValue]
 #[ORM\Column(type:'integer')]
 private $id;
---
+++ src/Entity/Task2.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\Task2Repository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: Task2Repository::class)]
+class Task2
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $description;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private $date;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $category;
+
+    #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'task2s')]
+    #[ORM\JoinColumn(nullable: false)]
+    private $user;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeImmutable
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeImmutable $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getCategory(): ?string
+    {
+        return $this->category;
+    }
+
+    public function setCategory(string $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
